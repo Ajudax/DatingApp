d048caa61a1e10383d83676c7737410177642f7e
Ajudax <peter.penning.jr@gmail.com>
2022-07-17 19:29:40 +0200

    End of section 9

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,24 @@
+import { JsonPipe } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_modules/member';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers(){
+    return this.http.get<Member[]>(this.baseUrl + 'users')
+  }
+
+  getMember(userName: string){
+    return this.http.get<Member>(this.baseUrl + 'users/' + userName)
+  }
+}
